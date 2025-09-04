ec5a1d4c96e00be4f3d96ed97c446e085b00d715
kwonboryong <kwonboryong@naver.com>
2025-09-04 15:46:58 +0900

    refactor: 중복 제출 방지를 위해 isSubmitting 상태 추가

--- src/hooks/useLoginForm.js
+++ src/hooks/useLoginForm.js
@@ -4,7 +4,8 @@ export const initialState = {
   email: '',
   password: '',
   showPassword: false,
-  warnings: { email: '', password: '', auth: '' }
+  warnings: { email: '', password: '', auth: '' },
+  isSubmitting: false
 };
 
 export function reducer(state, action) {
@@ -17,6 +18,8 @@ export function reducer(state, action) {
       return { ...state, showPassword: !state.showPassword };
     case 'SET_WARNING':
       return { ...state, warnings: { ...state.warnings, ...action.payload } };
+    case 'SET_SUBMITTING':
+      return { ...state, isSubmitting: action.payload };
     default:
       return state;
   }
